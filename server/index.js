012834edb0cca7bf3d407c9957b0dad05e5d52e6
Chris Kim <ckjr92@gmail.com>
2021-11-01 12:54:21 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley", "Chris"];
+
+app.get("/api/users", (req, res) => {
+    res.status(200).send(friends);
+})
+
+
+
+
+// server
+const SERVER_PORT = 4000;
+
+app.listen(SERVER_PORT, () => {
+    console.log(`You are tuned into ${SERVER_PORT}`)
+})
